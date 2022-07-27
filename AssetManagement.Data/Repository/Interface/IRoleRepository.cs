c0bc5d4f41432d99c5fcfd1878cffc675af127e0
Bùi Hà Nhi <buihanhi.dev@gmail.com>
2022-07-27 18:44:10 +0700

    initialize api

---
+++ AssetManagement.Data/Repository/Interface/IRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using AssetManagement.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Data.Repository.Interface
+{
+    public interface IRoleRepository
+    {
+        Task<Role> GetRoleByIdAsync(int id);
+    }
+}
