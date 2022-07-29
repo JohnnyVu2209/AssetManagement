f77cd5d24e643d62934fb39f470ce7f9f5c43999
tchtrong <tchtrong@apcs.vn>
2022-07-29 14:10:34 +0700

    Add more assets

--- AssetManagement.Data/AssetManagementDbContext.cs
+++ AssetManagement.Data/AssetManagementDbContext.cs
@@ -368,15 +368,69 @@ namespace AssetManagement.Data
                         Id = 1,
                         Code = "LA000001",
                         Name = "Laptop HP Probook 450 G1",
-                        Specification = "Dummy Spec",
+                        Specification = "Dummy Spec 1",
                         InstalledDate = DateTime.Now,
                         CategoryID = categories[0].Id,
                         StateID = states[1].Id,
                         LocationID = locations[0].Id
+                    },
+                    new Asset
+                    {
+                        Id = 2,
+                        Code = "MO000001",
+                        Name = "Moninor Dell UltraSharp",
+                        Specification = "Dummy Spec 2",
+                        InstalledDate = DateTime.Now,
+                        CategoryID = categories[0].Id,
+                        StateID = states[1].Id,
+                        LocationID = locations[1].Id
+                    },
+                    new Asset
+                    {
+                        Id = 3,
+                        Code = "PC000001",
+                        Name = "Personal Computer",
+                        Specification = "Dummy Spec 3",
+                        InstalledDate = DateTime.Now,
+                        CategoryID = categories[0].Id,
+                        StateID = states[1].Id,
+                        LocationID = locations[2].Id
+                    },
+                    new Asset
+                    {
+                        Id = 4,
+                        Code = "LA000002",
+                        Name = "Laptop HP Probook 450 G1",
+                        Specification = "Dummy Spec 4",
+                        InstalledDate = DateTime.Now,
+                        CategoryID = categories[0].Id,
+                        StateID = states[1].Id,
+                        LocationID = locations[0].Id
+                    },
+                    new Asset
+                    {
+                        Id = 5,
+                        Code = "MO000002",
+                        Name = "Moninor Dell UltraSharp",
+                        Specification = "Dummy Spec 5",
+                        InstalledDate = DateTime.Now,
+                        CategoryID = categories[0].Id,
+                        StateID = states[1].Id,
+                        LocationID = locations[1].Id
+                    },
+                    new Asset
+                    {
+                        Id = 6,
+                        Code = "PC000002",
+                        Name = "Personal Computer",
+                        Specification = "Dummy Spec 6",
+                        InstalledDate = DateTime.Now,
+                        CategoryID = categories[0].Id,
+                        StateID = states[1].Id,
+                        LocationID = locations[2].Id
                     }
                 )
             );
-
         }
     }
 }
