a48536e5780d8d8969101fe80f0adae23275e1dd
Khôn Vũ <vuhuong2000@outlook.com>
2022-08-03 21:33:02 +0700

    Rewrite unit test for view asset list

--- AssetManagement.Application.Tests/ControllersTests/AssetControllerTests.cs
+++ AssetManagement.Application.Tests/ControllersTests/AssetControllerTests.cs
@@ -5,11 +5,19 @@ using AssetManagement.Contracts.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 using Moq;
+using AssetManagement.Data;
+using AutoMapper;
+using AssetManagement.Contracts.AssetDTO;
+using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Http;
+using AssetManagement.Contracts;
 
 namespace AssetManagement.Application.Tests
 {
     public class AssetControllerTests
     {
+
         /*[Fact]
         public async void CanGetAllByCurrentAdminLocationAsync_NotFound()
         {
@@ -132,5 +140,101 @@ namespace AssetManagement.Application.Tests
             var assertValue = MockData.mapper.Map<AssetDetailDTO>(MockData.assets[0]);
             Assert.Equal(resultValue, assertValue);
         }*/
+        [Fact]
+        public async Task Test_Asset_GetAssetList_Return_Asset_List_With_Pagination()
+        {
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            var mockMapper = new Mock<IMapper>();
+            var mockUserManager = new Mock<FakeUserManager>();
+
+            var admin = new User()
+            {
+                Id = 1,
+                UserName = "Admin",
+                LocationId = 1,
+            };
+
+            var listAsset = GenerateAssetList(20);
+
+            var listAssetDto = ConvertDTO(listAsset);
+
+            AssetParameters parameters = new() { Searching = "la", Category = new() { 1 } };
+
+            mockUserManager.Setup(u => u.FindByNameAsync(It.IsAny<string>())).Returns(Task.FromResult(admin));
+
+            mockUnitOfWork.Setup(u => u.Assets.GetAllAsync()).Returns(Task.FromResult(listAsset.AsEnumerable()));
+
+            mockMapper.Setup(m => m.Map<List<AssetDTO>>(It.IsAny<List<Asset>>())).Returns(listAssetDto.Skip((parameters.PageNumber - 1) * parameters.PageSize).Take(parameters.PageSize).ToList());
+
+            var claimUser = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "Admin"),
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+            }, "mock"));
+
+            var identity = new GenericIdentity(ClaimTypes.Name, "AdminHCM");
+
+            identity.AddClaim(new Claim(ClaimTypes.Role, "Admin"));
+
+            var httpContext = new DefaultHttpContext()
+            {
+                User = claimUser,
+            };
+
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = httpContext,
+            };
+
+            AssetController controller = new(mockUnitOfWork.Object, mockMapper.Object, mockUserManager.Object) { ControllerContext = controllerContext };
+
+            var result = await controller.GetAllAsync(parameters) as OkObjectResult;
+
+            Assert.IsType<OkObjectResult>(result);
+
+            Assert.IsType<PageList<Asset, AssetDTO>>(result.Value);
+        }
+
+        private List<Asset> GenerateAssetList(int number)
+        {
+            var assetList = new List<Asset>();
+            for (int i = 1; i <= number; i++)
+            {
+                assetList.Add(new Asset()
+                {
+                    Id = i,
+                    Code =  $"LA{i.ToString().PadLeft(6,'0')}",
+                    Name = "Laptop HP Probook 450 G1",
+                    Specification = "Dummy Spec 1",
+                    InstalledDate = DateTime.Now,
+                    CategoryID = 1,
+                    Category = new Category() { Id = 1, Name = "Laptop"},
+                    StateID = 2,
+                    State = new State() { Id = 2, Name = "Available"},
+                    LocationID = 1,
+                    Location = new Location() { Id = 1, LocationName = "HCM"},
+                    CreatedDate = DateTime.Now,
+                    UpdatedDate = DateTime.Now
+                });
+            }
+            return assetList;
+        }
+
+        private List<AssetDTO> ConvertDTO(List<Asset> assets)
+        {
+            var assetListDto = new List<AssetDTO>();
+            foreach (var asset in assets)
+            {
+                assetListDto.Add(new()
+                {
+                    Name =asset.Name,
+                    Category = asset.Category.Name,
+                    State = asset.State.Name,
+                    Code  = asset.Code,
+                    Location = asset.Location.LocationName
+                });
+            }
+            return assetListDto;
+        }
     }
 }
